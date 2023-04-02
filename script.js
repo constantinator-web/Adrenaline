bebb0e9cff041ba778a4451151a907404f64caed
constantinator-web <constantinkotikov@mail.ru>
2023-04-02 19:40:02 +0300

    JS Switcher

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ if (menuBtn) {
     document.body.classList.toggle("lock");
   });
 }
-
+/*
 const sliderWraper = document.querySelector(".price__wraper");
 const slider = document.querySelector(".price__slider");
 const slides = document.querySelectorAll(".price__slider-card");
@@ -105,4 +105,51 @@ function clickHolder() {
 const order = parseInt(this.dataset.order, 10);
 activeOrder = order;
 update ();
-};  
+};  */
+
+const swiper = document.querySelector('.tracks__swiper');
+if (swiper) {
+  const trackSwitcherClosed = document.querySelector('.tracks__switcher-closed');
+  const trackSwitcherOpened = document.querySelector('.tracks__switcher-opened');
+  const paramsOpen = document.querySelectorAll('.tracks__item-param-open');
+  const paramsClose = document.querySelectorAll('.tracks__item-param-close');
+  const discriptonClose = document.querySelector('.tracks__discripton-map-close');
+  const discriptonOpen = document.querySelector('.tracks__discripton-map-open');
+  const controlNext = document.querySelector('.controlTracksNext');
+  const controlPrev = document.querySelector('.controlTracksPrev');
+  let SwitcheToClosed = function () {
+    trackSwitcherClosed.classList.add('switcher-active');
+    trackSwitcherOpened.classList.remove('switcher-active');
+    for (let i = 0; i < paramsOpen.length; i++) {
+      const param = paramsOpen[i];
+      param.style.display = "none";
+    }
+    for (let i = 0; i < paramsClose.length; i++) {
+      const param = paramsClose[i];
+      param.style.display = "block";
+    }
+    discriptonClose.classList.add('discripton-map-active');
+    discriptonOpen.classList.remove('discripton-map-active');
+  }; 
+  let SwitcheToOpened = function () {
+    trackSwitcherClosed.classList.remove('switcher-active');
+    trackSwitcherOpened.classList.add('switcher-active');
+    for (let i = 0; i < paramsOpen.length; i++) {
+      const param = paramsOpen[i];
+      param.style.display = "block";
+    }
+    for (let i = 0; i < paramsClose.length; i++) {
+      const param = paramsClose[i];
+      param.style.display = "none";
+    }
+    discriptonClose.classList.remove('discripton-map-active');
+    discriptonOpen.classList.add('discripton-map-active');  
+  };
+  trackSwitcherClosed.addEventListener('click', SwitcheToClosed); 
+  trackSwitcherOpened.addEventListener('click', SwitcheToOpened);
+  controlNext.addEventListener('click', SwitcheToOpened);
+  controlNext.addEventListener('click', function (e) {
+    controlNext.style.display = "none";
+    controlPrev.style.display = "block"
+  });
+}
